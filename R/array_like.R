8763f63caf2a53ace86859d019bde25cbf20b12f
Bradley Buchsbaum <bbuchsbaum@Umbrellabird.local>
2020-05-13 19:00:51 -0400

    array_like

--- R/array_like.R
+++ R/array_like.R
@@ -65,7 +65,7 @@ setMethod(f="[", signature=signature(x = "ArrayLike4D", i = "numeric", j = "nume
 setMethod(f="[", signature=signature(x = "ArrayLike4D", i = "numeric", j = "missing"),
           def=function (x, i, j, k, m, ..., drop=TRUE) {
             if (missing(k) && missing(m) && nargs() == 4) {
-              vals <- linear_access(x, i)
+              linear_access(x, i)
             } else {
               j <- seq(1, dim(x)[2])
               if (missing(k))
