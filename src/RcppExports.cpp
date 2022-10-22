9861ad9148dcce14d190e93204de82040c072e59
bbuchsbaum <brad.buchsbaum@gmail.com>
2022-10-22 15:18:48 -0400

    docs

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // indexToGridCpp
 NumericMatrix indexToGridCpp(IntegerVector idx, IntegerVector array_dim);
 RcppExport SEXP _neuroim2_indexToGridCpp(SEXP idxSEXP, SEXP array_dimSEXP) {
